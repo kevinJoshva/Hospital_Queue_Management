e191d658b9d78c45867f8660bac72c8016c66b64
dilepBabu <dilepbabu2003@gmail.com>
2025-12-12 12:21:54 +0530

    backend

---
+++ backend/HospitalQueueManagement/src/main/java/com/example/HospitalQueueManagement/HospitalQueueManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.HospitalQueueManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalQueueManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalQueueManagementApplication.class, args);
+	}
+
+}
